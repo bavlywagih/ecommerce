1786f35972d82911dde520b75553289add9c955c
bavlywagih <bavlywagih696@gmail.com>
2024-04-09 00:15:19 +0200

    Configure the codes v-5

--- Includes/Initialization/Init.php
+++ Includes/Initialization/Init.php
@@ -1,14 +1,13 @@
 <?php
 
     // routes
-
     $navbar ='Includes/Templates/Navbar/Navbar.php';
     $English ='Includes/languages/English/English.php';
     $Connect ='Includes/Connect/Conf.php';
     $Header = "Includes/Templates/Header/Header.php";
+    $Footer = "Includes/Templates/Footer/Footer.php";
 
-    $css ='layout/Css/';
-    $js = 'Layout/Js/';
+
 
 
     
---
+++ Layout/Js/All/All.js
@@ -0,0 +1,3 @@
+import '../Bootstrap/Bootstrap.min.js' ;
+import '../Jquery/Jquery-3.7.1.min.js' ;
+import '../Pages/Login/Login.js' ;
